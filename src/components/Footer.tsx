ee41d1f72c4f36905ce41d35aaee0a5b6d1fcd34
Maria Serykh <marie.serykh@gmail.com>
2023-03-18 22:51:32 +0100

    feat: add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import Container from './Container';
+import Logo from './Logo';
 
 class Footer extends Component {
   render() {
     return (
       <footer className="bg-amber-400 mt-auto">
-        <Container containerClass="py-4 flex gap-x-2 w-full items-center mx-auto">
+        <Container containerClass="py-4 flex flex-wrap gap-y-4 gap-x-2 w-full items-center justify-center mx-auto">
+          <div className="w-full flex flex-col content-start items-center">
+            <Logo />
+            <p className="text-white font-medium">Always a bargin</p>
+          </div>
           <nav>
-            <ul>
+            <ul className="flex gap-2">
               <li>
-                <NavLink to={'/'}>Main</NavLink>
+                <NavLink to={'/'} className="text-white p-2 hover:underline">
+                  Main
+                </NavLink>
               </li>
               <li>
-                <NavLink to={'/about'}>About</NavLink>
+                <NavLink to={'/about'} className="text-white p-2 hover:underline">About</NavLink>
               </li>
             </ul>
           </nav>
+          <div className="text-white font-medium flex gap-2">
+            <Link to="https://www.github.com/mserykh" className="text-white p-2 hover:underline">Mady by mserykh</Link>
+            <Link to="https://rs.school" className="text-white p-2 hover:underline">React course by RSSchool</Link>
+          </div>
         </Container>
       </footer>
     );
